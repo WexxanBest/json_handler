f2f2c1ee17d4f1add23f78f5d1569ffabbd41b0f
WexxanBest <kostya.hludeev@gmail.com>
2021-10-19 20:22:25 +0300

    Added LICENSE.txt

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@
 from setuptools import setup, find_packages
 
 from pathlib import Path
+
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
-
 
 setup(
     name='json_handler',
     version='1.1',
+    license='MIT',
     author='Wexxan',
     author_email='WexxanBest@yandex.ru',
     url='https://github.com/WexxanBest/json_handler',
@@ -19,9 +20,10 @@ setup(
     long_description_content_type='text/markdown',
     install_requires=[],
     packages=find_packages(),
-    classifiers=['Topic :: Utilities', 
+    classifiers=['Topic :: Utilities',
                  'Natural Language :: English',
                  'Operating System :: OS Independent',
+                 'License :: OSI Approved :: MIT License',
                  'Intended Audience :: Developers',
                  'Development Status :: 3 - Alpha',
                  'Programming Language :: Python :: 3.8'],
